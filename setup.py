d9c676562b123aae1c6005a51fafd2c1687012ba
Dan Gettings <daniel.p.gettings@gmail.com>
2014-10-23 11:08:15 -0400

    Produced a viable README.md file. Updated the "classifiers" keyword in setup.py

--- setup.py
+++ setup.py
@@ -28,11 +28,12 @@ setup(
 
     # license="BSD",
     # include_package_data = True
-    # requires=['numpy', 'pyfits', 'scipy'],
-    # install_requires=['numpy','pyfits','scipy']
-    # classifiers=[
-    #     #"Development Status :: 3 - Alpha",
-    #     #"Topic :: Scientific/Engineering :: Visualization",
-    #     # "License :: OSI Approved :: BSD License",
-    #     ]
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        #"License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2 :: Only"
+        ]
 )
